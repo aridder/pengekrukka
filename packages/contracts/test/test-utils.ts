802e55a0fadd1e45929148632706b12d3a356be8
Asbjørn Riddervold <30861967+aridder@users.noreply.github.com>
2022-12-08 08:29:24 +0100

    Monorepo (#14)
    * moved webapp into packages/webapp
    
    * added hardhat project from nb-boilerplate
    
    * extract common tsconfig which is extended in packages
    
    * gitignore globally in project instead of invidual
    
    * move .vscode
    
    * move .env.example files. Please move your .env.local to the root
    
    move .env.example files. Please move your .env.local to the root
    
    * update contract readme
    
    * remove unused deploy script task
    
    * one eslintrc
    
    * Debugger works with monorepo structure
    
    * Hardhat loads global .env file
    
    Co-authored-by: olaven <olav@sundfoer.com>

---
+++ packages/contracts/test/test-utils.ts
@@ -0,0 +1,72 @@
+import { Contract } from "ethers";
+import { deployments, ethers, network } from "hardhat";
+
+const getEtherConfig = () => {
+  const { NOK_ADDRESS, ME_ADDRESS, WHALE_ADDRESS } = process.env;
+
+  if (!NOK_ADDRESS || !ME_ADDRESS || !WHALE_ADDRESS) {
+    throw new Error("Missing env variables");
+  }
+
+  return { NOK_ADDRESS, ME_ADDRESS, WHALE_ADDRESS };
+};
+
+const impersonate = (...accounts: string[]) =>
+  Promise.all(
+    accounts.map((account) =>
+      network.provider.request({
+        method: "hardhat_impersonateAccount",
+        params: [account],
+      })
+    )
+  );
+
+const _transfer =
+  (contract: Contract) =>
+  async (options: { amount: number; sender: string; receiver: string }) => {
+    const signer = ethers.provider.getSigner(options.sender);
+    const tx = await contract
+      .connect(signer)
+      .transfer(options.receiver, options.amount);
+
+    await tx.wait();
+  };
+
+const _getBalance =
+  (contract: Contract) =>
+  (...accounts: string[]): Promise<number[]> =>
+    Promise.all(
+      accounts
+        .map((account) => contract.balanceOf(account))
+        .map(async (balance) => (await balance).toNumber())
+    );
+
+type ContractPath = `contracts/${string}.sol:${string}`;
+export const setup = async (contractPath: ContractPath) => {
+  const config = getEtherConfig();
+
+  // setting up local blockchain stuff
+  const contract = await ethers.getContractAt(
+    contractPath,
+    config.NOK_ADDRESS!
+  );
+
+  await impersonate(config.ME_ADDRESS, config.WHALE_ADDRESS);
+
+  // generating helper methods
+  const transfer = _transfer(contract);
+  const getBalance = _getBalance(contract);
+
+  return { config, contract, transfer, getBalance };
+};
+
+export const setupNOKToken = () => setup("contracts/CBContract.sol:CBToken");
+
+type DeployedContract = "TokenLock"; // | "More" | "In" | "The" | "Future"
+export const getDeployedContract = async (
+  name: DeployedContract
+): Promise<Contract> => {
+  await deployments.fixture(name);
+  const deployment = await deployments.get(name);
+  return ethers.getContractAt(deployment.abi, deployment.address);
+};
