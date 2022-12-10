a3da83aa35fe012352e5cb83d12c9e19442bcb28
aridder <aridder@pm.me>
2022-12-11 00:04:03 +0100

    Fix tests that didnt run because getContractAt wasnt waited for when called from in "describe" block. Also added tests for DidRegistry which let us add service endpoint

--- packages/contracts/test/CBToken.spec.ts
+++ packages/contracts/test/CBToken.spec.ts
@@ -3,69 +3,71 @@ import { expect } from "chai";
 import { setupNOKToken } from "./test-utils";
 
 describe("CBContract", async () => {
-  const { getBalance, transfer, config } = await setupNOKToken();
+  it("setup", async () => {
+    const { getBalance, transfer, config } = await setupNOKToken();
 
-  describe("Transfer", () => {
-    it("can access both accounts", async () => {
-      const [myAccount, whaleAccount] = await getBalance(
-        config.ME_ADDRESS,
-        config.WHALE_ADDRESS
-      );
-      expect(myAccount).not.to.be.undefined;
-      expect(whaleAccount).not.to.be.undefined;
-    });
-
-    it("decreases sender balance when transferring", async () => {
-      const [before] = await getBalance(config.WHALE_ADDRESS);
-
-      const amount = faker.datatype.number();
-      await transfer({
-        amount,
-        sender: config.WHALE_ADDRESS,
-        receiver: config.ME_ADDRESS,
+    describe("Transfer", () => {
+      it("can access both accounts", async () => {
+        const [myAccount, whaleAccount] = await getBalance(
+          config.ME_ADDRESS,
+          config.WHALE_ADDRESS
+        );
+        expect(myAccount).not.to.be.undefined;
+        expect(whaleAccount).not.to.be.undefined;
       });
 
-      const [after] = await getBalance(config.WHALE_ADDRESS);
+      it("decreases sender balance when transferring", async () => {
+        const [before] = await getBalance(config.WHALE_ADDRESS);
 
-      expect(after).to.equal(before - amount);
-    });
+        const amount = faker.datatype.number();
+        await transfer({
+          amount,
+          sender: config.WHALE_ADDRESS,
+          receiver: config.ME_ADDRESS,
+        });
 
-    it("increases the receiver balance when transferring", async () => {
-      const [before] = await getBalance(config.ME_ADDRESS);
+        const [after] = await getBalance(config.WHALE_ADDRESS);
 
-      const amount = faker.datatype.number();
-      await transfer({
-        amount,
-        sender: config.WHALE_ADDRESS,
-        receiver: config.ME_ADDRESS,
+        expect(after).to.equal(before - amount);
       });
 
-      const [after] = await getBalance(config.ME_ADDRESS);
+      it("increases the receiver balance when transferring", async () => {
+        const [before] = await getBalance(config.ME_ADDRESS);
 
-      expect(after).to.equal(before + amount);
-    });
+        const amount = faker.datatype.number();
+        await transfer({
+          amount,
+          sender: config.WHALE_ADDRESS,
+          receiver: config.ME_ADDRESS,
+        });
 
-    it("Does not transfer if sender does not have the funds available", async () => {
-      const [meBefore, whaleBefore] = await getBalance(
-        config.ME_ADDRESS,
-        config.WHALE_ADDRESS
-      );
-      expect(
-        transfer({
-          amount: meBefore + 1, //i.e. more than is available
-          sender: config.ME_ADDRESS,
-          receiver: config.WHALE_ADDRESS,
-        })
-      ).rejectedWith(Error);
+        const [after] = await getBalance(config.ME_ADDRESS);
 
-      const [meAfter, whaleAfter] = await getBalance(
-        config.ME_ADDRESS,
-        config.WHALE_ADDRESS
-      );
+        expect(after).to.equal(before + amount);
+      });
 
-      //i.e. no change
-      expect(meAfter).equal(meBefore);
-      expect(whaleAfter).equal(whaleBefore);
+      it("Does not transfer if sender does not have the funds available", async () => {
+        const [meBefore, whaleBefore] = await getBalance(
+          config.ME_ADDRESS,
+          config.WHALE_ADDRESS
+        );
+        expect(
+          transfer({
+            amount: meBefore + 1, //i.e. more than is available
+            sender: config.ME_ADDRESS,
+            receiver: config.WHALE_ADDRESS,
+          })
+        ).rejectedWith(Error);
+
+        const [meAfter, whaleAfter] = await getBalance(
+          config.ME_ADDRESS,
+          config.WHALE_ADDRESS
+        );
+
+        //i.e. no change
+        expect(meAfter).equal(meBefore);
+        expect(whaleAfter).equal(whaleBefore);
+      });
     });
   });
 });
