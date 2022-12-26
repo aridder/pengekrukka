c316a280a92bda34e80686e1fc5b56ab7be4bf7a
olaven <olav@sundfoer.com>
2022-12-26 15:11:25 +0100

    Sharing code between tests

--- packages/webapp/src/server/trpc/router/welfare.ts
+++ packages/webapp/src/server/trpc/router/welfare.ts
@@ -1,30 +1,13 @@
-import { generateVC, VCConfig } from "@pengekrukka/vc-shared";
-import { TRPCError } from "@trpc/server";
+import { generateVC } from "@pengekrukka/vc-shared";
+import { getConfig } from "../../../utils/config";
 import { router, validations } from "../trpc";
 import { protectedProcedure } from "./../trpc";
-
-const getConfig = (): VCConfig => {
-  const fromEnv = [process.env.WELFARE_MNEMONIC, process.env.RPC_URL];
-
-  if (fromEnv.every((e) => !!e)) {
-    return {
-      mnemonic: process.env.WELFARE_MNEMONIC as string,
-      rpcUrl: process.env.RPC_URL as string,
-    };
-  } else {
-    throw new TRPCError({
-      code: "INTERNAL_SERVER_ERROR",
-      message:
-        "Configuration was not in expected state: Something was undefined",
-    });
-  }
-};
 
 export const welfareRouter = router({
   getWelfareVc: protectedProcedure
     .input(validations.publicKey)
     .query(async ({ input }) => {
-      const config = getConfig();
+      const config = getConfig("WELFARE_MNEMONIC");
 
       //FIXME: some user id validation and lookup of actual welfare amount
       // TODO add revocation and type of credential from a config file or something
