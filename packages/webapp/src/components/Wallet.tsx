b844fcc47c089ae6330ae49d0c1822b3495c2e99
olaven <olav@sundfoer.com>
2023-01-05 12:57:20 +0100

    Fix Hydration Error
    This PR removes the hydration error that occurs on
    re-render of the page. This allows us to avoid nav-bar
    navigation in the demo :)

--- packages/webapp/src/components/Wallet.tsx
+++ packages/webapp/src/components/Wallet.tsx
@@ -1,9 +1,24 @@
+"use client";
 import { VerifiableCredential } from "@pengekrukka/vc-shared";
 import { useCallback, useEffect, useState } from "react";
-import { ethers } from "ethers";
 import { useAccount } from "wagmi";
-import { trpc } from "../utils/trpc";
 import { formatDate, shortenDid } from "../utils";
+import { trpc } from "../utils/trpc";
+
+function ClientOnly({ children }: { children: React.ReactNode }) {
+  // State / Props
+  const [hasMounted, setHasMounted] = useState(false);
+
+  // Hooks
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  // Render
+  if (!hasMounted) return null;
+
+  return <div>{children}</div>;
+}
 
 export default function Wallet() {
   const { address, isConnected } = useAccount();
@@ -23,26 +38,28 @@ export default function Wallet() {
   return (
     <div className="flex h-full flex-col items-center space-y-8 bg-green-600">
       <p className="mt-4">Mine digitale bevis</p>
-      {!isConnected && <p>Du må være logget inn for å se dine digitale bevis</p>}
-      {isConnected && (
-        <div>
-          <button onClick={getVc}>Hent bevis</button>
-          {myVerifiableCredentials.map((vc, index) => {
-            return (
-              <div
-                key={index}
-                className="flex h-36 w-3/4 flex-col items-center justify-center rounded-xl bg-slate-400"
-              >
-                <div className="flex flex-col">
-                  {vc.type && <p className="text-s font-bold">{vc.type[vc.type.length - 1]}</p>}
-                  <p>{formatDate(vc.issuanceDate)}</p>
-                  {vc.credentialSubject.id && <p>{shortenDid(vc.credentialSubject.id)}</p>}
+      <ClientOnly>
+        {!isConnected && <p>Du må være logget inn for å se dine digitale bevis</p>}
+        {isConnected && (
+          <div>
+            <button onClick={getVc}>Hent bevis</button>
+            {myVerifiableCredentials.map((vc, index) => {
+              return (
+                <div
+                  key={index}
+                  className="flex h-36 w-3/4 flex-col items-center justify-center rounded-xl bg-slate-400"
+                >
+                  <div className="flex flex-col">
+                    {vc.type && <p className="text-s font-bold">{vc.type[vc.type.length - 1]}</p>}
+                    <p>{formatDate(vc.issuanceDate)}</p>
+                    {vc.credentialSubject.id && <p>{shortenDid(vc.credentialSubject.id)}</p>}
+                  </div>
                 </div>
-              </div>
-            );
-          })}
-        </div>
-      )}
+              );
+            })}
+          </div>
+        )}
+      </ClientOnly>
     </div>
   );
 }
