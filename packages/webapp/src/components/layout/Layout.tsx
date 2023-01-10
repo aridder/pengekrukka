01391d943eb3903a505212c7c5818ddddd83b5ef
olaven <olav@sundfoer.com>
2023-01-10 13:47:27 +0100

    Added Header, but without navigation
    I removed the header in #69. However, I've realized that it's useful to have
    some common UI showing wether the user is logged in or not. Therefore, I've
    added it back, but removed the navigation links.
    
    Merge if you agree. If not, reach out :)

--- packages/webapp/src/components/layout/Layout.tsx
+++ packages/webapp/src/components/layout/Layout.tsx
@@ -1,3 +1,4 @@
+import Header from "../header";
 import Wallet from "../Wallet";
 
 interface Props {
@@ -6,11 +7,14 @@ interface Props {
 
 export default function Layout({ children }: Props) {
   return (
-    <div className="flex h-full w-full flex-row justify-between">
-      <div className="h-full w-4/5">{children}</div>
-      <div className="h-screen w-1/5">
-        <Wallet />
+    <>
+      <Header />
+      <div className="flex h-full w-full flex-row justify-between">
+        <div className="h-full w-4/5">{children}</div>
+        <div className="h-screen w-1/5">
+          <Wallet />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
