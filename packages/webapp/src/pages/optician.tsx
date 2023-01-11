ec4f69bf0a8561370e1a558f3e7b49e8763cbab3
Olav Sundfør <olav@sundfoer.com>
2023-01-11 21:41:14 +0100

    Added component for glasses (#84)
    Closes #51

--- packages/webapp/src/pages/optician.tsx
+++ packages/webapp/src/pages/optician.tsx
@@ -1,9 +1,12 @@
 import Layout from "../components/layout/Layout";
+import { Glasses } from "../components/optician/Glasses";
 
 export default () => {
   return (
     <Layout>
-      <div>TODO: optician's store</div>;
+      <div>TODO: optician's store</div>
+      <h1 className="text-6xl">Hansens Brilleforetning</h1>
+      <Glasses price={2500} />
     </Layout>
   );
 };
