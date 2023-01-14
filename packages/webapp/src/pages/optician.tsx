5e0fe993ed67ceb0549a1899b029bcc7af3d7c97
olaven <olav@sundfoer.com>
2023-01-14 11:19:03 +0100

    Welfare amount is deducted from total
    Closes https://github.com/aridder/pengekrukka/issues/54
    + `UploadSection` only showing relevant cards

--- packages/webapp/src/pages/optician.tsx
+++ packages/webapp/src/pages/optician.tsx
@@ -1,11 +1,20 @@
+import React, { useState } from "react";
 import { useAccount } from "wagmi";
 import Layout from "../components/layout/Layout";
 import { Glasses } from "../components/optician/Glasses";
 import { UploadSection } from "../components/UploadSection";
-import { ClientOnly } from "../components/utils";
-import { isWelfareCredential, VerifiableCredential } from "../server/trpc/schemas";
+import { Button, ClientOnly } from "../components/utils";
+import {
+  isWelfareCredential,
+  VerifiableCredential,
+  WelfareCredential,
+} from "../server/trpc/schemas";
+import { VerifiableCredentialType } from "../server/trpc/vc-shared";
 
-const DiscountSection = (props: { address: string | undefined }) => {
+const DiscountSection = (props: {
+  address: string | undefined;
+  onDiscount: (credential: WelfareCredential) => void;
+}) => {
   if (!props.address) {
     return <p>Logg inn for å legge til støtte</p>;
   }
@@ -14,24 +23,54 @@ const DiscountSection = (props: { address: string | undefined }) => {
     if (!isWelfareCredential(credential)) {
       alert("Kun brillestøtte tillat");
     } else {
-      alert("Skal legge til stoette paa " + credential.credentialSubject.amount);
+      props.onDiscount(credential);
     }
   };
 
-  return <UploadSection address={props.address} onCredentialSelected={onUpload} />;
+  return (
+    <UploadSection
+      address={props.address}
+      onCredentialSelected={onUpload}
+      type={VerifiableCredentialType.WelfareCredential}
+    />
+  );
 };
 
 export default () => {
   const { address, isConnected } = useAccount();
+  const [discountCredential, setDiscountCredential] = useState<WelfareCredential | null>(null);
+
+  const PRICE = 2500;
+  const [total, setTotal] = React.useState(PRICE);
+
+  React.useEffect(() => {
+    if (discountCredential) {
+      setTotal(PRICE - discountCredential.credentialSubject.amount);
+    }
+  }, [discountCredential?.credentialSubject.amount]);
 
   return (
     <Layout>
       <div>TODO: optician's store</div>
       <h1 className="text-6xl">Hansens Brilleforetning</h1>
-      <Glasses price={2500} />
-      <ClientOnly>
-        <DiscountSection address={address} />
-      </ClientOnly>
+      <Glasses price={PRICE} />
+      {!discountCredential && (
+        <ClientOnly>
+          <DiscountSection address={address} onDiscount={setDiscountCredential} />
+        </ClientOnly>
+      )}
+      {discountCredential && (
+        <div className="text-gray-500">
+          <b>Støtte: </b>
+          <span>
+            {PRICE} - {discountCredential.credentialSubject.amount}
+          </span>
+        </div>
+      )}
+      <div className="my-8 flex">
+        <p>Total: {total}</p>
+        <Button className="mx-4">Kjøp</Button>
+      </div>
     </Layout>
   );
 };
