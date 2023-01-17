0b4cbaa20af7e7c60436a2928c04c77b0af7196a
olaven <olav@sundfoer.com>
2023-01-17 10:24:47 +0100

    Building lib as part of build command

--- packages/contracts/src/circuits/index.ts
+++ packages/contracts/src/circuits/index.ts
@@ -4,4 +4,4 @@ import _withdraw from "./withdraw.json";
 const _proving_key = fs.readFileSync("./withdraw_proving_key.bin").buffer;
 
 export const proving_key = _proving_key;
-export const withdraw = _withdraw;
+export const withdraw: any = _withdraw;
